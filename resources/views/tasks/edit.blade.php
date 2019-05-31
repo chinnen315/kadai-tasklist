6c6ac53f5242c71831c63e57457344590644da4a
takuya.hamada <thamada@s-pri.co.jp>
2019-05-31 10:41:07 +0000

    add column

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,9 +6,14 @@
 <div class="row">
     <div class="col-6">
         {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-    
+
             <div class="form-group">
-                {!! Form::label('content', 'メッセージ:') !!}
+                {!! Form::label('status', 'ステータス:') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+
+            <div class="form-group">
+                {!! Form::label('content', 'タスク内容:') !!}
                 {!! Form::text('content', null, ['class' => 'form-control']) !!}
             </div>
     
